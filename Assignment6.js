48f1c27871aa8725b4291b413e352a48baffa343
ezazsabit <sabit20.kuet@gmail.com>
2022-03-02 14:37:49 +0600

    first commit

---
+++ Assignment6.js
@@ -0,0 +1,132 @@
+//------------function for fetch  the selected item
+const showOutout=searchItem=>{
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchItem}`)
+    .then(res=>res.json())
+    .then(data=>showCards(data.data))}
+
+//---function for showing others
+const others=values=>{
+   if(values==undefined)
+   {
+       //console.log('no data')
+       return 'no data found'
+   }
+   else{
+    const othersArray=[];
+    for(const [key,value] of Object.entries(values))
+    {
+        othersArray.push(`${key} = ${value}`)
+    }
+    return (othersArray.join(' || '))
+   }
+}
+
+//validity check
+const validItem=item=>{
+    if(item==undefined)
+    {
+        return 'no data found'
+    }
+    else{
+        return item;
+    }
+}
+
+
+ //-------------function for showing the details of an item 
+ const showDetail=item=>{
+     
+const detailsection=document.getElementById('detail-item');
+    const div=document.createElement('div');
+    detailsection.textContent='';
+    div.innerHTML=`<div class=" row card" >
+    <img src="${item.data.image}" class="card-img-top img-fluid col-10 col-lg-6 h-50 w-50" alt="...">
+    <div class="card-body col-10 col-lg-6 w-100">
+      
+        <h3 class="card-title">${validItem(item.data.name)}</h3>
+        
+     
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><h5>Realease date :</h5> ${validItem(item.data.releaseDate)}</li>
+        <li class="list-group-item"><h5>Sensors:</h5> ${validItem(item.data.mainFeatures.sensors.join(','))}</li>
+        
+        <li class="list-group-item"><h5>Others: </h5>${others(item.data.others)
+        }</li>
+        
+      </ul>
+    </div>
+
+    
+  </div>`
+   detailsection.appendChild(div)  
+ } 
+
+ 
+ 
+//--------- functon for showing the product in cards-
+const showCards=items=>{
+    // console.log(item)
+    if(items.length===0)
+    {
+        
+        const Section=document.getElementById('cardsItem');
+        document.getElementById('cardsItem').textContent='';
+        const div=document.createElement('div');
+        div.innerHTML=`<h3>No item matched...</h3>`
+        div.setAttribute("style", "display: flex; justify-content: center;");
+        Section.appendChild(div)
+    }
+    else{
+    document.getElementById('cardsItem').textContent='';
+    for(const item of items.slice(0,20))
+    {
+        
+        const Section=document.getElementById('cardsItem');
+        const div=document.createElement('div');
+        div.classList.add('col-10')
+        div.classList.add('col-lg-4')
+        div.classList.add('col-md-6')
+        div.innerHTML=` <div class="card mt-5 mx-auto  rounded w-100 ms-3" style="width: 18rem; background-color:rgb(240, 195, 195);">
+        <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h3 class="card-title">${item.phone_name}</h3>
+          <h6 class="card-title">${item.brand}</h6>
+        
+          <p id="${item.slug}"  class="btn btn-secondary">Details</p>
+         
+          
+        </div>
+      </div>`;
+      Section.appendChild(div)
+      document.getElementById(`${item.slug}`).addEventListener('click',function(){
+          scrollTop();
+         
+        fetch(`https://openapi.programming-hero.com/api/phone/${item.slug}`)
+        .then(response=>response.json())
+        .then(details=>showDetail(details)) 
+      })
+    }}
+}
+//scroll to the top
+var scrollTop = function() {
+    window.scrollTo(0, 0);
+};
+
+
+
+
+
+
+
+//----main operation
+document.getElementById('searchButton').addEventListener('click',function(){
+    document.getElementById('detail-item').textContent='';
+    const searchItem=(document.getElementById('searchInput').value.toLowerCase());//case sensitivity solve
+    showOutout(searchItem);
+    document.getElementById('searchInput').value='';
+   
+})
+
+
+
+
